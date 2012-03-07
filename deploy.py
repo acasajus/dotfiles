6a99932dd39ae12d2d6af20c64e580bd15594384
Adrià Casajús <adria@ecm.ub.es>
2012-03-07 19:16:57 +0100

    Added deploy script

---
+++ deploy.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os, pickle, re, logging, sys
+try:
+  from hashlib import md5
+except:
+  from md5 import md5
+
+logging.basicConfig( level = logging.INFO )
+
+userDataRE = re.compile( "\$\{([\w\s]+)\}" )
+
+here = os.path.dirname( os.path.realpath( __file__ ) )
+
+stateFile = os.path.join( here, ".dotstate" )
+try:
+  fd = open( stateFile )
+  dotState = pickle.load( fd )
+  fd.close()
+except:
+  dotState = {}
+
+def getContentHash( path ):
+  logging.debug( "Getting contents from %s" % path )
+  pathFile = open( sourcePath )
+  data = pathFile.read()
+  pathFile.close()
+  return ( data,  md5( data ).hexdigest() )
+
+for module in os.listdir( here ):
+  if module[0] == "." or module == "deploy.py":
+    continue
+  logging.info( "Exploring %s" % module )
+  modPath = os.path.join( here, module )
+  #Execute module deploy if it's there
+  modDeploy = os.path.join( modPath, "deploy.py" )
+  if os.path.isfile( modDeploy ):
+    logging.info( "Found %s/deploy.py, executing..." % module )
+    if os.system( modDeploy ) != 0:
+      logging.error( "Error executing %s/deploy.py. Exiting" % module )
+      sys.exit( 1 )
+    continue
+  #Copyi/link files where they are needed
+  for source in os.listdir( modPath ):
+    sourcePath = os.path.join( modPath, source )
+    logging.info( "Processing %s/%s" % ( module, source ) )
+    if not os.path.isfile( sourcePath ):
+      continue
+    destPath = os.path.expanduser( os.path.join( "~" , ".%s" % source ) )
+    logging.debug( "Destination is %s" % destPath )
+    sData, sHash = getContentHash( sourcePath )
+    if os.path.isfile( destPath ):
+      dData, dHash = getContentHash( sourcePath )
+      if sHash == dHash or dotState.get( sHash, "" ) == dHash :
+        logging.info( "%s/%s already in sync" % ( module, source ) )
+        continue
+    #Need to sync the file
+    #Any templating?
+    logging.info( "Syncing %s to %s" % ( sourcePath, destPath ) )
+    requiredData = userDataRE.findall( sData )
+    if requiredData:
+      logging.info( "%s/%s requires %s user info" % ( module, source, ", ".join( requiredData ) ) )
+      dData = sData
+      for dataPiece in requiredData:
+        value = raw_input( "Value for %s -> %s? " % ( destPath, dataPiece ) )
+        dData = dData.replace( "${%s}" % dataPiece, value )
+      dHash = md5( dData ).hexdigest()
+    if os.path.exists( destPath ):
+      os.unlink( destPath )
+    destFile = open( destPath, "w" )
+    destFile.write( dData )
+    destFile.close()
+    dotState[ sHash ] = dHash
+
+fd = open( stateFile, "w" )
+pickle.dump( dotState, fd )
+fd.close()
