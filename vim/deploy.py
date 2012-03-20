a56f7cd2955c4da4bff43a4bdf8cbbded7c52494
Adrià Casajús <adria@ecm.ub.es>
2012-03-20 18:22:51 +0100

    Changed buffergator for minibufexpl

--- vim/deploy.py
+++ vim/deploy.py
@@ -11,17 +11,26 @@ if not os.path.isdir( vimDir ):
 print os.path.join( vimDir, ".git" )
 if os.path.isdir( os.path.join( vimDir, ".git" ) ):
   print "Seems janus is already installed in %s" % vimDir
-  sys.exit( 0 )
+else:
+  for name in ( ".vim", ".vimrc", ".gvimrc" ):
+    nPath = os.path.expanduser( os.path.join( "~" , name ) )
+    if os.path.isfile( nPath ):
+      os.rename( nPath, "%s.old" % nPath )
 
-for name in ( ".vim", ".vimrc", ".gvimrc" ):
-  nPath = os.path.expanduser( os.path.join( "~" , name ) )
-  if os.path.isfile( nPath ):
-    os.rename( nPath, "%s.old" % nPath )
+  if os.system( "git clone https://github.com/carlhuda/janus.git %s" % vimDir ) != 0:
+    print "Cound not clone janus into %s" % vimDir
+    sys.exit( 1 )
+
+  if os.system( "cd %s && rake" % vimDir ) != 0:
+    print "Could not execute rake"
+
+#Install custom plugins
+janusDir = os.path.expanduser( os.path.join( "~" , ".janus" ) )
+if not os.path.isdir( janusDir ):
+  os.makedirs( janusDir )
 
 
-if os.system( "git clone https://github.com/carlhuda/janus.git %s" % vimDir ) != 0:
-  print "Cound not clone janus into %s" % vimDir
+if os.system( "git clone https://github.com/fholgado/minibufexpl.vim.git %s" % os.path.join( janusDir, "minibufexpl" ) ) != 0:
+  print "Cound not clone minibufexpl into %s" % janusDir
   sys.exit( 1 )
 
-if os.system( "cd %s && rake" % vimDir ) != 0:
-  print "Could not execute rake"
