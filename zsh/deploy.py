42551d6eaaf62d6a3579353feddc315d061513d2
Adrià Casajús <adria@ecm.ub.es>
2012-04-20 18:32:16 +0200

    Fix zsh deploy

--- zsh/deploy.py
+++ zsh/deploy.py
@@ -4,22 +4,17 @@ import sys, os
 
 here = os.path.dirname( os.path.realpath( __file__ ) )
 
-zDir = os.path.expanduser( os.path.join( "~", ".zsh" ) )
-if not os.path.exists( zDir ):
-  os.symlink( os.path.join( here, "zsh" ), zDir )
-
 omzDir = os.path.expanduser( os.path.join( "~" , ".oh-my-zsh" ) )
 if os.path.isdir( omzDir ):
   print "Seems oh-my-zsh is already installed in %s" % omzDir
-  sys.exit( 0 )
+else:
+  os.environ[ "GIT_SSL_NO_VERIFY"] = "true"
 
-os.environ[ "GIT_SSL_NO_VERIFY"] = "true"
+  if os.system( "git clone https://github.com/acasajus/oh-my-zsh.git %s" % omzDir ) != 0:
+    print "Cound not clone oh-my-zsh into %s" % omzDir
+    sys.exit( 1 )
 
-if os.system( "git clone https://github.com/acasajus/oh-my-zsh.git %s" % omzDir ) != 0:
-  print "Cound not clone oh-my-zsh into %s" % omzDir
-  sys.exit( 1 )
-
-if os.system( "cd %s; git submodule update --init --recursive" % omzDir ) != 0:
-  print "Could not initialize OMZ submodules"
-  sys.exit( 1 )
+  if os.system( "cd %s; git submodule update --init --recursive" % omzDir ) != 0:
+    print "Could not initialize OMZ submodules"
+    sys.exit( 1 )
 
