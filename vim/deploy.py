6284caee6c5ba19e34dd5b2c415f7fbe36a22eb6
Adrià Casajús <adria@ecm.ub.es>
2012-10-07 22:59:46 +0200

    Fixed Vundle installation

--- vim/deploy.py
+++ vim/deploy.py
@@ -6,7 +6,7 @@ here = os.path.dirname( __file__ )
 
 for link, target in ( ( ".vimrc", os.path.join( here, "vimrc" ) ), ( ".vim", os.path.join( here, "vim" ) ) ):
   target = os.path.realpath( target )
-  link = os.path.expanduser( os.path.join( "~", link ) ) 
+  link = os.path.expanduser( os.path.join( "~", link ) )
   if os.path.exists( link ):
     if os.path.islink( link ):
       if os.readlink( link ) == target:
@@ -22,5 +22,7 @@ for dName in ( "backups", "swaps", "undo" ):
   dName = os.path.expanduser( os.path.join( "~", ".vim", dName ) )
   if not os.path.isdir( dName ):
     os.makedirs( dName )
-  
-os.system( "vim +BundleInstall +q" )
+
+os.system( "git clone https://github.com/gmarik/vundle.git '%s'" % ( os.path.join( here, "vim", "bundle", "vundle" ) ) )
+
+os.system( "vim +BundleInstall +qa" )
